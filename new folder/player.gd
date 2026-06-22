06a6e20b937f8d8b5000a058e1e741ee64e18a6e
Ewan303 <ewanz.currie@gmail.com>
2026-06-21 19:22:08 -0500

    init

--- new folder/player.gd
+++ new folder/player.gd
@@ -18,15 +18,17 @@ enum PLAYERSTATE {
 
 
 # Constant velocities
-const FALL_V := 500.0
-const WALK_V := 200.0
-const JUMP_V := 500.0
-const DASH_V = 600.0
-const WALLSLIDE_V = 500.0
+const FALL_V := 700.0
+const WALK_V := 300.0
+const JUMP_V := 550.0
+const DASH_V := 600.0
+const WALLSLIDE_V := 500.0
+const WALLJUMP_V := 550.0
 
 # Other constants
 const JUMP_D := 1000.0
-const DASH_LEN = 200.0
+const DASH_LEN := 200.0
+const WALLJUMP_LEN := 30.0
 
 # Variables
 var activeState := PLAYERSTATE.FALL
@@ -34,7 +36,7 @@ var facingDir := 1.0
 var canDash := false
 var dashJumpBuffer := false
 var GRAV: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-var WALLSLIDE_GRAV: float = (ProjectSettings.get_setting("physics/2d/default_gravity"))/2
+var WALLSLIDE_GRAV: float = (ProjectSettings.get_setting("physics/2d/default_gravity"))/4
 
 
 # Update character with func _physics_process and _ready
@@ -69,7 +71,10 @@ func switch_state(state: PLAYERSTATE) -> void:
 				return
 			sprite.play("dash")
 			velocity.y = 0
-
+		PLAYERSTATE.WALLSLIDE:
+			sprite.play("wallslide")
+			velocity.y = 0
+			
 func process_state(delta: float) -> void:
 	match activeState:
 		PLAYERSTATE.FALL:
@@ -79,7 +84,8 @@ func process_state(delta: float) -> void:
 				switch_state(PLAYERSTATE.FLOOR)
 			elif Input.is_action_just_pressed("jump") and airjumptimer.time_left > 0:
 				switch_state(PLAYERSTATE.JUMP)
-
+			elif is_input_toward_facing() and can_wall_slide():
+				switch_state(PLAYERSTATE.WALLSLIDE)
 		PLAYERSTATE.FLOOR:
 			if Input.get_axis("move_left","move_right"):
 				sprite.play("walk")
@@ -93,9 +99,16 @@ func process_state(delta: float) -> void:
 		PLAYERSTATE.JUMP:
 			velocity.y = move_toward(velocity.y,0,JUMP_D * delta)
 			movement()
-			
 			if Input.is_action_just_released("jump") or velocity.y >= 0:
 				velocity.y = 0
+				switch_state(PLAYERSTATE.FALL)
+		PLAYERSTATE.WALLSLIDE:
+			velocity.y = move_toward(velocity.y,WALLSLIDE_V,WALLSLIDE_GRAV * delta)
+			movement()
+			
+			if is_on_floor():
+				switch_state(PLAYERSTATE.FLOOR)
+			elif not can_wall_slide():
 				switch_state(PLAYERSTATE.FALL)
 # Movement of player character
 func movement() -> void:
@@ -105,8 +118,11 @@ func movement() -> void:
 		facingDir = inputDir
 		rayWallSlide.position.x = inputDir * absf(rayWallSlide.position.x)
 		rayWallSlide.target_position.x = inputDir * absf(rayWallSlide.target_position.x)
-		rayWallSlide
+		rayWallSlide.force_raycast_update()
 	velocity.x = inputDir * WALK_V
 
 func is_input_toward_facing() -> bool:
 	return signf(Input.get_axis("move_left","move_right")) == facingDir
+
+func can_wall_slide() -> bool:
+	return is_on_wall_only() and rayWallSlide.is_colliding()
